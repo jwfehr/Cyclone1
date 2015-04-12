d10616383b5a218158645f9de84a6f6c00a2b1ff
jwfehr <justinfehr@Justins-MacBook-Pro.local>
2015-04-12 18:00:38 -0500

    Creates Init File and updates main accordingly

---
+++ init.h
@@ -0,0 +1,14 @@
+/**
+ * Initialization Function - A single function that calls the init functions for all necessary components
+ * @author Jacob Johnson, Justin Fehr, Mitchell Borman, Richard Millan, Zach Bennett
+ * @date 4/12/2015
+ */
+
+#ifndef INIT_H
+#define INIT_H
+
+///Initialize Function, Calls the functions to initialize all necessary functions
+void initAll(oi_t *self);
+
+
+#endif // INIT_H
