6a15c6d378074afa53ad16a5267cf58972f4e67d
Jacob <jacob.johnson.public@gmail.com>
2015-04-12 15:56:12 -0500

    Updated Music
    added correct commenting and a header header file

---
+++ music.h
@@ -0,0 +1,17 @@
+#ifndef MUSIC_H
+#define BLUETOOTH_H
+
+#include <stdlib.h>
+#include "util.h"
+#include "open_interface.h"
+
+/**
+ * Music Playback API Header - Provides a set of functions for controlling bluetooth communications
+ * @author Jacob Johnson, Justin Fehr, Mitchell Borman, Richard Millan, Zach Bennett
+ * @date 4/12/2015
+ */
+
+/// Sets Up a Variety of Songs
+void load_coin_sound(int index);
+
+#endif // MUSIC_H
