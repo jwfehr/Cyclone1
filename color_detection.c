6a1d92d56d5fb0ad362dd33bfeed7baf2da03768
zwbce <zwbce@iastate.edu>
2015-04-13 23:13:30 -0500

    Added lcd files and a color detection tester

---
+++ color_detection.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+#include "lcd.h"
+#include "util.h"
+#include "open_interface.h"
+
+void main( void ) {
+	lcd_init();
+	while(1) {
+		oi_update(sensor);
+		int left_cliff = sensor->cliff_left;
+		int right_cliff = sensor->cliff_right;
+		int left_signal = sensor->cliff_left_signal;
+		int right_signal = sensor->cliff_right_signal;
+		
+		lprintf("Left: %d\nSignal: %d\nRight: %d\nSignal: %d", left_cliff, left_signal, right_cliff, right_signal);
+	}
+}
