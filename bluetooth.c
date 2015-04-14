9be9e4e4dde2cf559326f4c3c9c1fc1a83739b89
Jacob <jacob.johnson.public@gmail.com>
2015-04-13 19:38:49 -0500

    Bluetooth Comments
    Finished commenting the bluetooth files.

--- bluetooth.c
+++ bluetooth.c
@@ -1,39 +1,55 @@
 /**
- * Bluetooth Control API - Provides a set of functions for controlling bluetooth communications
+ * Bluetooth Control API - provides a set of functions for controlling bluetooth communications via USART
  * @author Jacob Johnson, Justin Fehr, Mitchell Borman, Richard Millan, Zach Bennett
- * @date 4/12/2015
+ * @date 4/13/2015
  */
 
 /// Initialize Bluetooth Transmission
+/**
+ * Initilization function used to setup USART registers for bluetooth communication
+ * @param baud opcode representing the baudrate to be used for serial communication
+ */
 void init_bluetooth(unsigned long baud)
 {
-	UBRR0H = (unsigned char)(baud >> 8);
+	UBRR0H = (unsigned char)(baud >> 8);    // set baud to inserted parameter
 	UBRR0L = (unsigned char)(baud);
-	UCSR0A = 0b00000010;
-	UCSR0C = 0b00001110;
-	UCSR0B = 0b00011000;
+	UCSR0A = 0b00000010;                    // enable double transmission speeds
+	UCSR0C = 0b00001110;                    // 8 bit data frame and 2 bit stop
+	UCSR0B = 0b00011000;                    // enable transmition and reception
 }
 
 /// Transmit a Single Character
+/**
+ * Trasmition function used to send a single 8 bit character via bluetooth
+ * @param data the 8 bit character to be sent
+ */
 void serial_putc(char data)
 {
-	while((UCSR0A & 0b00100000) == 0);
-	UDR0 = data;
+	while((UCSR0A & 0b00100000) == 0);      // while previous transmittion is not complete
+	UDR0 = data;                            // place data value in trasnmittion register
 }
 
 /// Transmit a Formatted String
+/**
+ * Trasmition function used to send a string of 8 bit characters via bluetooth
+ * @param string of 8 bit characters to be sent
+ */
 void serial_puts(char x[])
 {
     int i;
-	for(i = 0; i < strlen(x); i++)
+	for(i = 0; i < strlen(x); i++)          // for each character in the string
 	{
-		serial_putc(x[i]);
+		serial_putc(x[i]);                  // send that character using seriel_putc
 	}
 }
 
 /// Receive a Single Character
+/**
+ * Reception function used to recieve a single 8 bit character via bluetooth
+ * @return the 8 bit character recieved
+ */
 char serial_getc()
 {
-	while((UCSR0A & 0b10000000) == 0);
-	return UDR0;
+	while((UCSR0A & 0b10000000) == 0);      // while reception is not complete
+	return UDR0;                            // return the recieved character
 }
--- bluetooth.h
+++ bluetooth.h
@@ -1,22 +1,38 @@
-/** 
- * Bluetooth Control API Header - Provides a set of function headers for controlling bluetooth communications
+/**
+ * Bluetooth Control API - provides a set of functions for controlling bluetooth communications via USART
  * @author Jacob Johnson, Justin Fehr, Mitchell Borman, Richard Millan, Zach Bennett
- * @date 4/12/2015
+ * @date 4/13/2015
  */
 
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
 /// Initialize Bluetooth Transmission
+/**
+ * Initilization function used to setup USART registers for bluetooth communication
+ * @param baud opcode representing the baudrate to be used for serial communication
+ */
 void init_bluetooth(unsigned long baud);
 
 /// Transmit a Single Character
+/**
+ * Trasmition function used to send a single 8 bit character via bluetooth
+ * @param data the 8 bit character to be sent
+ */
 void serial_putc(char data);
 
 /// Transmit a Formatted String
+/**
+ * Trasmition function used to send a string of 8 bit characters via bluetooth
+ * @param string of 8 bit characters to be sent
+ */
 void serial_puts(char x[]);
 
 /// Receive a Single Character
+/**
+ * Reception function used to recieve a single 8 bit character via bluetooth
+ * @return the 8 bit character recieved
+ */
 char serial_getc();
 
 #endif // BLUETOOTH_H
